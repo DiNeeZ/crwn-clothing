4b449c432b1f4a902ee6bea0b5f1aa47cf20ae16
DiNeeZ <dneez86@gmail.com>
2022-04-03 14:37:52 +0300

    cheking

--- src/components/category-item/category-item.component.jsx
+++ src/components/category-item/category-item.component.jsx
@@ -10,7 +10,7 @@ const CategoryItem = ({ categories }) => {
       }}></div>
       <div className='category-body-container'>
         <h2>{title}</h2>
-        <p>Shop Now</p>
+        <p>Shop Now Proverka</p>
       </div>
     </div>
   )
